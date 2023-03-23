088bd575eb9536ecdd3aab84a831e9ef269930c6
ARBAZ <khanarbaz0674@gmail.com>
2023-03-23 11:07:22 +0530

    Created Booking Routes and Function

--- routes/bookingRoutes.js
+++ routes/bookingRoutes.js
@@ -8,10 +8,20 @@ const bookingController = require('./../controllers/bookingController');
 
 const router = express.Router();
 
-router.get(
-  '/checkout-session/:tourID',
-  authController.protect,
-  bookingController.getCheckoutSession
-);
+router.use(authController.protect);
+
+router.get('/checkout-session/:tourID', bookingController.getCheckoutSession);
+
+router.use(authController.restrictTo('admin', 'lead-guide'));
+
+router
+  .route('/')
+  .get(bookingController.getAllBookings)
+  .post(bookingController.createBooking);
+
+router
+  .route('/:id')
+  .get(bookingController.getBooking)
+  .patch(bookingController.deleteBooking);
 
 module.exports = router;
